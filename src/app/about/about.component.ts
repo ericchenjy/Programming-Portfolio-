2a64c0dde2cb249237897cb7f6899c3a049690a6
Eric Chen <ericchen@Erics-MacBook-Pro-2.local>
2024-11-27 02:37:07 -0500

    updated home page and about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { workExperiences } from '../about/work-experiences';
 
 @Component({
   selector: 'app-about',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
+
+  screenSize: boolean = window.innerWidth < 900;
+
+  workExperiences = workExperiences;
+
+  constructor(private router: Router){
+  }
+  
+  pageRouter(path: string): void{
+    this.router.navigate([path]);
+  }
+
+
+  @HostListener('window:resize',['$event'])
+  screenSizeChecking(event: Event): void{
+    this.screenSize = window.innerWidth < 900;
+  }
 
 }
--- src/app/about/work-experiences.ts
+++ src/app/about/work-experiences.ts
@@ -2,49 +2,53 @@ export const workExperiences =  [
 
     {
         work_logo:"assets/Images/peerly-logo.png",
-        work_title: "Front End Developer",
-        work_dates: "April 2022 - January 2024",
+        work_title: "Front End Developer - UI/UX Designer",
+        work_dates: "Apr 2022 - Jan 2024",
         work_location: "Burlington, Ontario, Canada",
         work_commpany: "Peerly",
-        work_description: "Projects Worked on: Peerly Job Application, Peerly Text Message Labeling, Peerly Admin Paltform, Peerly Agent Platform. Skills: Angular Framework, Ionic Framework, Google Firebase, Node.js, Typescript, Javascript, RxJS, Rest API, HTML, SCSS,Bootstrap, Git, Figma, Material Design"
+        work_description: "Angular/Ionic Framework, Google Firebase, Node.js, Typescript, Javascript, RxJS, Rest API, HTML, SCSS,Bootstrap, Git, Figma, Material Design"
 
     },
 
     {
         work_logo: "assets/Images/University of Guelph.png",
         work_title: "Junior Analysis Internship ",
-        work_dates: "January 2021 - August 2021",
+        work_dates: "Jan 2021 - Aug 2021",
         work_location: "Guelph, Ontario, Canada",
         work_commpany: "University of Guelph",
         work_description: "Worked on GyphForms (BP Logix), Quality Assurance Testing, Hardware Troubleshooting, IT Support, Data Erasure"
 
     },
 
+    
+
     {
         work_logo: "assets/Images/University of Guelph.png",
         work_title: "Instructional and Technical Support Assistant Internship",
-        work_dates: "June 2020 - August 2020",
+        work_dates: "Jun 2020 - Aug 2020",
         work_location: "Guelph, Ontario, Canada",
         work_commpany: "Univeristy of Guelph",
-        work_description: "Provided Zoom/Microsoft Teams support for Online Teaching, Formatting/Designing course outlines, Developed/Deisgned infographics using Canvas for educational strategies, managed work tasks with Trellow and Microsoft Planner"
+        work_description: "Zoom/Microsoft Teams Support, Formatting/Designing Course outlines, Developed/Designed infographics (Canvas), Trello, Microsoft Planner"
 
     },
 
     {
-        work_name: "",
-        work_dates: "",
-        work_location: "",
-        work_commpany: "",
-        work_description: ""
+        work_logo:"assets/Images/Health Canada.png",
+        work_title: "Web Publisher Internship",
+        work_dates: "Sep 2019 - Dec 2019",
+        work_location: "Ottawa, Ontario, Canada",
+        work_commpany: "Health Canada",
+        work_description: "Web Content, HTML, Adobe Dreamweaver, Drupal Technical Support"
 
     },
 
     {
-        work_name: "",
-        work_dates: "",
-        work_location: "",
-        work_commpany: "",
-        work_description: ""
+        work_logo: "assets/Images/kazaMeza.avif",
+        work_title: "Restaurant Team Member",
+        work_dates: "May 2019 - Aug 2019",
+        work_location: "Guelph, Ontario, Canada",
+        work_commpany: "KazaMeza",
+        work_description: "Customer Service, Kitchen Helper, Cleaner"
 
     }
 
